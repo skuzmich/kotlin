b2807ab7f4ea3463ea1e9518f2b464c257e0de56
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2019-02-18 16:26:50 +0300

    Add intention to introduce import alias  #KT-16118 Fixed  #KT-30007 Fixed

---
+++ idea/testData/intentions/introduceImportAlias/notApplicableAlias.kt
@@ -0,0 +1,15 @@
+// IS_APPLICABLE: false
+import Outer.Inner as NotTestAlias
+
+class Outer {
+    class Inner
+}
+
+class Test(){
+    fun test(){
+        val i = NotTestAlias<caret>()
+    }
+    fun test2(){
+        val i = NotTestAlias()
+    }
+}
---
+++ idea/testData/intentions/introduceImportAlias/notApplicableLocalClass.kt
@@ -0,0 +1,8 @@
+// IS_APPLICABLE: false
+
+class Test(){
+    fun test(){
+        class Foo
+        val i = Foo<caret>()
+    }
+}
